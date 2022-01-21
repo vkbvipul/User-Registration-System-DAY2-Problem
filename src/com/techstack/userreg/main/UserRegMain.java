d9cf111b45831921ba18e63989de5ecfc4f98e44
Vipul Bhardwaj <vkb_vipul@hotmail.com>
2022-01-21 15:22:39 +0530

    UC3 commit.

--- src/com/techstack/userreg/main/UserRegMain.java
+++ src/com/techstack/userreg/main/UserRegMain.java
@@ -1,23 +1,27 @@
 package com.techstack.userreg.main;
-
 public class UserRegMain {
-
 	public static void main(String[] args) {
 		// User Registration System needs to ensure all validations are in place during the User Entry
 		String firstName = "Sdde";
-		String lastName = "dde";
+		String lastName = "DDde";
+		String email = "abc.xyz@bl.co.in";
 		
 		//UC1
 		if(checkLengthAndFirstChar(firstName,firstName.length()))
 		 System.out.println("FirstName should be of more than 3 chars and should have first char uppercase.");
-		else
-		 System.out.println("FirstName is correct !!, UC 1 passed.");
-		
 		//UC2
 		if(checkLengthAndFirstChar(lastName,lastName.length()))
 		 System.out.println("LastName should be of more than 3 chars and should have first char uppercase.");
-		else
-		 System.out.println("LastName is correct !!, UC 1 passed.");
+		//UC3
+		if(!checkValidityOfEmail(email))
+			System.out.println("Email is not correct ! UC3");
+	}
+
+	private static boolean checkValidityOfEmail(String email) {
+		String regex = "^[A-Za-z0-9+_.-]+@bl.co(.+)$";
+		if(email.matches(regex))
+			return true;
+		return false;
 	}
 
 	private static boolean checkLengthAndFirstChar(String firstName, int length) {
