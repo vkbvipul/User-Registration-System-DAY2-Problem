58acd5f306b0e68efeb6b15b171c154793ed0bbe
Vipul Bhardwaj <vkb_vipul@hotmail.com>
2022-01-21 12:27:52 +0530

    Initial Commit

---
+++ src/com/techstack/userreg/main/UserRegMain.java
@@ -0,0 +1,11 @@
+package com.techstack.userreg.main;
+
+public class UserRegMain {
+
+	public static void main(String[] args) {
+		// User Registration System needs to ensure all validations are in place during the User Entry
+		
+
+	}
+
+}
