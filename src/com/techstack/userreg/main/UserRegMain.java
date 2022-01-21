2d2be3621790234aef692eda04235d20d0e276a2
Vipul Bhardwaj <vkb_vipul@hotmail.com>
2022-01-21 13:54:11 +0530

    UC1 completed.

--- src/com/techstack/userreg/main/UserRegMain.java
+++ src/com/techstack/userreg/main/UserRegMain.java
@@ -4,8 +4,18 @@ public class UserRegMain {
 
 	public static void main(String[] args) {
 		// User Registration System needs to ensure all validations are in place during the User Entry
-		
-
+		String firstName = "Abcde";
+		int length = firstName.length();
+		if(length < 3) {
+			System.out.println("Name length is shorter than 3 chars.");
+			return;
+		}
+		else if (!Character.isUpperCase(firstName.charAt(0)))
+		{
+			System.out.println("First character is not UPPERCASE.");
+			return;
+		}
+		System.out.println("Name is correct !!, UC 1 passed.");
 	}
 
 }
